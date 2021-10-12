bd03e3732134f512da672aec87959a7202f97ef5
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-10-12 20:44:58 +0200

    Revert "add models"
    This reverts commit cb12cc86d31034dca6c2a5627a4c0fe75cce27d2.

--- app.js
+++ app.js
@@ -6,20 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var coolRouter = require('./routes/cool');
+
 var app = express();
-
-// get user info from module
-const userinfo = require('./userinfo');
-const password = encodeURIComponent(userinfo.pass);
-const uri = `mongodb://rishaan:${password}@cluster0-shard-00-00.nektk.mongodb.net:27017,cluster0-shard-00-01.nektk.mongodb.net:27017,cluster0-shard-00-02.nektk.mongodb.net:27017/local_library?ssl=true&replicaSet=atlas-h6huv8-shard-0&authSource=admin&retryWrites=true&w=majority`;
-// setup mongoDB
-
-const mongoose = require('mongoose');
-const mongoDB = uri;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,15 +21,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/cool', coolRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
